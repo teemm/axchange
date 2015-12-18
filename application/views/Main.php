1e28e4e13b266a198080ba4d34d349f3f9d1a702
teemm <kverna133@gmail.com>
2015-12-19 01:48:18 +0400

    codeigniter

---
+++ application/views/Main.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta carset="utf-8">
+</head>
+<body>
+
+</body>
+</html>
