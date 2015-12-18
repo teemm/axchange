3f9e7dc4434a9bb46bac84b5c620c0b1e51cdbb4
teemm <kverna133@gmail.com>
2015-12-19 02:22:24 +0400

    add some styles

--- application/views/Main.php
+++ application/views/Main.php
@@ -1,13 +1,37 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 	<meta carset="utf-8">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?> ">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css'); ?> ">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/reset.css'); ?> ">
+	<script src="<?php echo base_url('/assets/js/jquery-2.1.4.min.js'); ?>"></script>
 </head>
 <body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+  <!--   <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div> -->
 
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 </body>
 </html>
