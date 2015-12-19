8669cbdd555f4923cf148c6e446b97578765bece
teemm <kverna133@gmail.com>
2015-12-19 18:42:00 +0400

    edit bugs

--- application/views/vip.php
+++ application/views/vip.php
@@ -1,24 +1,34 @@
-<h1 class="text-center">VIP განცხადებები</h1>
+
 	<div class="vip-cl">
-		<div class="vip-images">
-			<a href="">
-				<img src="<?php echo base_url('assets/images/swaps/1.jpg'); ?>" alt="..." class="img-thumbnail">
-			</a>
-			<h2 class="vipTitle">unchage - 3 დისკი</h2>
-			<p class="vipPar"> გაიცლვება - last of რაღაცის დისკში </p>
-		</div>
-		<div class="vip-images">
-		<a href="">
-				<img src="<?php echo base_url('assets/images/swaps/2.jpg'); ?>" alt="..." class="img-thumbnail">
-			</a>
-			<h2 class="vipTitle">unchage - 3 დისკი</h2>
-			<p class="vipPar"> გაიცლვება - last of რაღაცის დისკში </p>
-		</div>
-		<div class="vip-images">
-		<a href="">
-				<img src="<?php echo base_url('assets/images/swaps/3.jpg'); ?>" alt="..." class="img-thumbnail">
-			</a>
-			<h2 class="vipTitle">unchage - 3 დისკი</h2>
-			<p class="vipPar"> გაიცლვება - last of რაღაცის დისკში </p>
-		</div>
+		<h1 class="text-center">VIP განცხადებები</h1>
+			<div class="vip-images">
+					<div class="inline">
+						<p class="fix">
+							<a href="">
+								<img class="vipimg" src="<?php echo base_url('assets/images/swaps/1.jpg'); ?>" alt="..." class="img-thumbnail">
+							</a>
+						</p>
+						<h2 class="vipTitle">unchage - 3 დისკი</h2>
+						<p class="vipPar"> გაიცლვება - last of რაღაცის დისკში </p>
+					</div>
+					<div class="inline">
+						<p class="fix">
+							<a href="">
+								<img class="vipimg" src="<?php echo base_url('assets/images/swaps/2.jpg'); ?>" alt="..." class="img-thumbnail">
+							</a>
+						</p>
+						<h2 class="vipTitle">unchage - 3 დისკი</h2>
+						<p class="vipPar"> გაიცლვება - last of რაღაცის დისკში </p>
+					</div>
+					
+					<div class="inline">
+						<p class="fix">
+							<a href="">
+								<img class="vipimg" src="<?php echo base_url('assets/images/swaps/3.jpg'); ?>" alt="..." class="img-thumbnail">
+							</a>
+						</p>
+						<h2 class="vipTitle">unchage - 3 დისკი</h2>
+						<p class="vipPar"> გაიცლვება - last of რაღაცის დისკში </p>
+					</div>
+			</div>
 	</div>
