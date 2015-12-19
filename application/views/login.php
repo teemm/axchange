aef8a2f6be7f92b4f9d255f7fe20818cee5116f4
geolabmisha <mikheil.tsankashvili02@geolab.edu.ge>
2015-12-19 18:10:59 +0400

    დაემატა რეგისტრაციის და ავტორიზაციის ვიზუალური მხარე

--- application/views/login.php
+++ application/views/login.php
@@ -1,7 +1,7 @@
-<div class="login">
-	<form action="<?php echo base_url('Registration/loginpanel'); ?>" method="post">		
-		<p><input type="email" placeholder="ელ-ფოსტა" name="email" /></p>
-		<p><input type="password" name="password" /></p>
-		<p><input type="submit" value="შესვლა" /></p>
+<div class="formblock">
+	<form action="<?php echo base_url('Registration/loginpanel'); ?>" method="post" class="loginform">		
+		<p><span class="fa fa-envelope-o"></span><input type="email" placeholder="ელ-ფოსტა" name="email"  class="loginemail textinpt" /></p>
+		<p><span class="fa fa-lock"></span><input type="password" name="password" placeholder="პაროლი"  class="loginpassword textinpt"/></p>
+		<p><input type="submit" value="შესვლა" class="submitbutton transition"/></p>
 	</form>
 </div>
