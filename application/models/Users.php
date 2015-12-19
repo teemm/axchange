f600cdd32d6312893c739fa42a920f7198d3421a
geolabmisha <mikheil.tsankashvili02@geolab.edu.ge>
2015-12-19 12:10:18 +0400

    დაემატა რეგისტრაცია controller

--- application/models/Users.php
+++ application/models/Users.php
@@ -2,8 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Users extends CI_Model{
-	public function login(){
-		$this->db->select();
-		
+	public function register(){
+		$data = array(
+	        'fname' => htmlentities($this->input->post('fname',TRUE)),
+	        'email' => htmlentities($this->input->post('email',TRUE)),
+	        'user_img'=>'user-alt-128.png',
+	        'password'=> htmlentities($this->input->post('password',TRUE)),
+	        'mobnumber'=> htmlentities($this->input->post('mobnumber',TRUE)),
+		);
+		$this->db->insert('users', $data);
+		$this->session->set_flashdata('sucess', 'თქვენ წარმატებით დარეგისტრირდით');
 	}
 }
